061b0054f83ea07c798e4722ad525f711fe1b6cd
hifi <andreas.rosenhahn@gmail.com>
2022-03-06 23:55:28 +0100

    headr

--- api/modules/sk_header/header.php
+++ api/modules/sk_header/header.php
@@ -1,6 +1,7 @@
 <?php
 
-header('Access-Control-Allow-Origin: http://localhost:8084');
+header('Access-Control-Allow-Origin: http://localhost:8080');
+// header('Access-Control-Allow-Origin: http://countdown.spacekit.de');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
